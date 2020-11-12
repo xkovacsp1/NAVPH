01cf193af17eb0017ac71dd6843dcc97631212eb
xkovacsp1 <patrikkovi@gmail.com>
2020-11-12 19:52:18 +0100

    player tank added

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class Player : MonoBehaviour
+    {
+      
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+        
+        }
+    }
+}
