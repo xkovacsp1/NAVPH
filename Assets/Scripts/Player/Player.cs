59ee52571bcc522cc346ca0e389b3a65381c06be
xkovacsp1 <patrikkovi@gmail.com>
2020-11-13 17:42:10 +0100

    player movement added

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,16 +4,63 @@ namespace Assets.Scripts.Player
 {
     public class Player : MonoBehaviour
     {
-      
-        void Start()
-        {
+        public float speed = 12f;                  // How fast the tank moves forward and back.
+        public float turnSpeed = 180f;             // How fast the tank turns in degrees per 
 
+        public string MovementAxisName { get; set; } // The name of the input axis for moving forward and back.
+        public string TurnAxisName { get; set; } // The name of the input axis for turning.
+        public Rigidbody RigidBody { get; set; } // Reference used to move the tank.
+        public float MovementInputValue { get; set; } // The current value of the movement input.
+        public float TurnInputValue { get; set; } // The current value of the turn input.
+
+        private void Awake()
+        {
+            RigidBody = GetComponent<Rigidbody>();
+        }
+
+        private void OnEnable()
+        {
+            RigidBody.isKinematic = false;
+            MovementInputValue = 0f;
+            TurnInputValue = 0f;
+        }
+
+        private void OnDisable()
+        {
+            RigidBody.isKinematic = true;
+        }
+
+        private void Start()
+        {
+            MovementAxisName = "Vertical";
+            TurnAxisName = "Horizontal";
         }
 
 
-        void Update()
+        private void Update()
         {
-        
+            MovementInputValue = Input.GetAxis(MovementAxisName);
+            TurnInputValue = Input.GetAxis(TurnAxisName);
         }
+
+        private void FixedUpdate()
+        {
+            Move();
+            Turn();
+        }
+
+        private void Move()
+        {
+            var movement = transform.forward * MovementInputValue * speed * Time.deltaTime;
+            RigidBody.MovePosition(RigidBody.position + movement);
+        }
+
+        private void Turn()
+        {
+            float turn = TurnInputValue * turnSpeed * Time.deltaTime;
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+            RigidBody.MoveRotation(RigidBody.rotation * turnRotation);
+        }
+
     }
 }
