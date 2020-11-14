408f5c3ece9f5e87f76eb1fba270e717a312f4d1
xkovacsp1 <patrikkovi@gmail.com>
2020-11-14 13:12:27 +0100

    added enemy spawning by specific areas

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 namespace Assets.Scripts.Enemy
 {
     public class EnemySpawner : MonoBehaviour
     {
         public List<EnemyBehaviourContext> EnemyBehaviors { get; } = new List<EnemyBehaviourContext>();
-        public float zPos=20.18f;
-        public float yPos;
-        public float maxXPos = 14.87f;
-        public float minXPos = -15.16f;
-        public int enemyNumber = 1;
 
+        public List<EnemySpawnPoint> SpawnPoints { get; set; } = new List<EnemySpawnPoint>()
+        {
+            {new EnemySpawnPoint(14.39f, false)},
+            { new EnemySpawnPoint(9.27f, false)},
+            { new EnemySpawnPoint(4.5f, false)},
+            {new EnemySpawnPoint(-0.45f, false)},
+            {new EnemySpawnPoint(-5.53f, false)},
+            { new EnemySpawnPoint(-10.32f, false)}
+        };
+
+        public float zPos = 20.18f;
+        public float yPos;
+        //public float maxXPos = 14.87f;
+        //public float minXPos = -15.16f;
+        public int enemyNumber = 6;
+        public Random Random { get; } = new Random();
 
         public void Start()
         {
@@ -21,33 +33,23 @@ namespace Assets.Scripts.Enemy
 
         private IEnumerator MakeEnemies()
         {
-            for (var i = 0; i < enemyNumber; i++)
+            var enemyCount = 0;
+            while (enemyCount < enemyNumber)
             {
-                //var enemyTiger = Resources.Load("prefabs/EnemyTiger", typeof(GameObject)) as GameObject;
-                var enemyTiger = Instantiate(Resources.Load("prefabs/EnemyTiger", typeof(GameObject)) as GameObject);
-                if (enemyTiger)
+                var index = Random.Next(SpawnPoints.Count);
+                var spawnPoint = SpawnPoints[index];
+                if (!spawnPoint.IsActive)
                 {
+                    var enemyTiger = Instantiate(Resources.Load("prefabs/EnemyTiger", typeof(GameObject)) as GameObject);
                     enemyTiger.transform.rotation = gameObject.transform.rotation;
-                    enemyTiger.transform.position = new Vector3(Random.Range(minXPos, maxXPos), yPos, zPos);
+                    enemyTiger.transform.position = new Vector3(spawnPoint.XPos, yPos, zPos);
+                    SpawnPoints[index].IsActive = true;
                     IEnemyBehaviour enemyBehavior = new TigerEnemy(enemyTiger);
                     EnemyBehaviourContext context = new EnemyBehaviourContext(enemyBehavior);
                     EnemyBehaviors.Add(context);
+                    enemyCount++;
                 }
-
-                yield return new WaitForSeconds(1f);
-
-                //if (i < 5)
-                //{
-                //    enemy.name = "Drone";
-                //    enemyBehavior = new DroneBehavior(Time.time, enemy, minX, maxX);
-                //    enemy.GetComponent<MeshRenderer>().material.color = Color.blue;
-                //}
-                //else
-                //{
-                //    enemy.name = "Fighter";
-                //    enemyBehavior = new FighterBehavior(Time.time, enemy, minX, maxX);
-                //    enemy.GetComponent<MeshRenderer>().material.color = Color.red;
-                //}
+                yield return new WaitForSeconds(3f);
             }
         }
 
